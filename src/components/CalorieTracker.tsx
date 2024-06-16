14bd87c88108ddf138989a08624a006366da222b
marco-soria <marco.soriaa7@gmail.com>
2024-06-16 18:13:40 -0500

    first commit

---
+++ src/components/CalorieTracker.tsx
@@ -0,0 +1,29 @@
+import CalorieDisplay from "./CalorieDisplay"
+import { useActivity } from "../hooks/useActivity"
+
+export default function CalorieTracker() {
+
+    const { caloriesConsumed, caloriesBurned, netCalories } = useActivity()
+    
+    return (
+        <>
+            <h2 className="text-4xl font-black text-white text-center">Calories Summarys</h2>
+
+            <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
+                <CalorieDisplay
+                    calories={caloriesConsumed}
+                    text="Consumed"
+                />
+                <CalorieDisplay
+                    calories={caloriesBurned}
+                    text="Burned"
+                />
+                <CalorieDisplay
+                    calories={netCalories}
+                    text="Difference"
+                />
+            </div>
+ 
+        </>
+    )
+}
